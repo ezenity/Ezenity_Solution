338dd09e3489cca6bf85b1ec699ed754b77e19fc
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-20 17:12:07 -0500

    Add project files.

---
+++ Ezenity_React/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ezenity_React
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    // This will allow to you to override the default builder using the fluent APIs
+                    webBuilder.UseContentRoot("some-path");
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
