c6089c7d2915ec26788e54c9ce7abe32deef2b28
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-21 19:23:03 -0500

    Feat: Creating Middleware extension
     - This allows us to add the middleware to the pipeline

---
+++ Ezenity_React/MiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Ezenity_React
+{
+    public static class MiddlewareExtensions
+    {
+        /**
+         * This method is used to register the middleware class.
+         * 
+         * By making the middleware available in an instance of IApplicationBuilder,
+         * it can be added to the pipeline in the Configure method within the 
+         * startup class.
+         */
+        public static IApplicationBuilder UseCustomLogger(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CustomLogger>();
+        }
+    }
+}
