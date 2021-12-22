f70b40bf5078fd46bbc4260c08fff7d307f4b63c
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-21 19:18:36 -0500

    Docs: Details regarding Invoke Method

--- Ezenity_React/CustomLogger.cs
+++ Ezenity_React/CustomLogger.cs
@@ -13,6 +13,13 @@ namespace Ezenity_React
             _next = next ?? throw new ArgumentNullException(nameof(next));
         }
 
+        /**
+         * This will get executed in the request/response pipeline.
+         * 
+         * The request details will log at the start of the Invoke method and log 
+         * the response details after the _next delegate has been executed, which
+         * will be when the rest of the pipeline has been executed.
+         */
         public async Task Invoke(HttpContext httpContext)
         {
             if (httpContext == null) throw new ArgumentNullException(nameof(HttpContext));
