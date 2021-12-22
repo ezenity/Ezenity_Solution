f75eb2a0616f3e50f4073e3f7e0ccd25b730b949
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-21 19:12:46 -0500

    Feat: Create own middleware
     - This class will log information about every single request that is handled by the ASP.NET Core App

---
+++ Ezenity_React/CustomLogger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Ezenity_React
+{
+    public class CustomLogger
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomLogger(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext == null) throw new ArgumentNullException(nameof(HttpContext));
+
+            // TODO - log the request
+
+            await _next(httpContext);
+
+            // TODO - log the response
+        }
+    }
+}
