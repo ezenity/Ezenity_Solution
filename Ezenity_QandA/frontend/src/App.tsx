b182482ce8a23881b89edc9716f978ad980f1805
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-30 18:07:08 -0500

    Feat: consme fist React component

--- Ezenity_QandA/frontend/src/App.tsx
+++ Ezenity_QandA/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './Header';
 
 function App() {
   //const App: React.FC = () => {
@@ -8,20 +8,7 @@ function App() {
     //debugger;
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload. Here is a edit.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Header />
         </div>
     );
   //};
--- Ezenity_QandA/frontend/src/Header.tsx
+++ Ezenity_QandA/frontend/src/Header.tsx
@@ -1,5 +1,22 @@
 ﻿import React from 'react';
+import logo from './logo.svg';
 
 // const: Allows you to declare and initialize a variable where its reference won't change later in the program
 // let: allows you to delcare a variable where its reference can change later in the program
-export const Header = () => <div>header</div>;
+// export const Header = () => <div>Header<div/>
+export const Header = () => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>
+      Edit <code>src/App.tsx</code> and save to reload. Here is a edit.
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn React
+    </a>
+  </header>
+);
