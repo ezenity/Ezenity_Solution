8f7d23ab0523102ea680e53448e96c54da2217c0
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-30 18:29:38 -0500

    Feat: Creating another component

--- Ezenity_QandA/frontend/src/App.tsx
+++ Ezenity_QandA/frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Header } from './Header';
+import { HomePage } from './HomePage';
 
 function App() {
   //const App: React.FC = () => {
-    //const unused = 'something';
-    //debugger;
-    return (
-        <div className="App">
-            <Header />
-        </div>
-    );
+  //const unused = 'something';
+  //debugger;
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
   //};
 }
 
@@ -25,7 +27,7 @@ export default App;
 // // referred to as refs within the React community.
 // // It is not recommended to implement this type of
 // // usage.
-// this React ref feature allows us to access the DOM node. 
+// this React ref feature allows us to access the DOM node.
 // // More Info here: https://reactjs.org/docs/refs-and-the-dom.html
 //class ProblemComponent extends React.Component {
 //    render() {
---
+++ Ezenity_QandA/frontend/src/HomePage.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+export const HomePage = () => (
+  <div>
+    <div>
+      <h2>Unanswered Questions</h2>
+      <button>Ask a question</button>
+    </div>
+  </div>
+);
