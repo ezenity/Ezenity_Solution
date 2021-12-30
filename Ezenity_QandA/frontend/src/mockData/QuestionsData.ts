5d17cc94f8aa7d823fcbd9c27489cdda0948ce0b
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-30 18:40:11 -0500

    Feat: Answer data interface

--- Ezenity_QandA/frontend/src/mockData/QuestionsData.ts
+++ Ezenity_QandA/frontend/src/mockData/QuestionsData.ts
@@ -1,6 +1,16 @@
-﻿export interface Questiondata {
+﻿// Defines the structure of the questions we expect to be working with
+export interface QuestionData {
   questionId: number;
   title: string;
+  content: string;
+  userName: string;
+  created: Date;
+  answers: AnswerData[];
+}
+
+// Interface for the structure of the answers we're expceting
+export interface AnswerData {
+  answerId: number;
   content: string;
   userName: string;
   created: Date;
