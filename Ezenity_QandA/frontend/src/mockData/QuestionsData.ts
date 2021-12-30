f6669b3fe0850ccfa4643863404a91d6fbbce7c3
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-30 18:48:43 -0500

    Feat: Adding in mock data

--- Ezenity_QandA/frontend/src/mockData/QuestionsData.ts
+++ Ezenity_QandA/frontend/src/mockData/QuestionsData.ts
@@ -15,3 +15,36 @@ export interface AnswerData {
   userName: string;
   created: Date;
 }
+
+// Mock questions
+const questions: QuestionData[] = [
+  {
+    questionId: 1,
+    title: 'Why should I learn TypeScript?',
+    content: 'TypeScript seems to be getting popular so I wondered whether it is worth my time learning it? What benefits does it give over JavaScript?',
+    userName: 'Anthony',
+    created: new Date(),
+    answers: [
+      {
+        answerId: 1,
+        content: 'To catch problems earlier speeding up your developments',
+        userName: 'Ant',
+        created: new Date(),
+      },
+      {
+        answerId: 2,
+        content: 'So, that you can use the JavaScript features of tomorrow, today',
+        userName: 'Mac',
+        created: new Date(),
+      },
+    ],
+  },
+  {
+    questionId: 2,
+    title: 'Which state management tool should I use?',
+    content: 'there seem t obe a fair few state management tools around for React - React, unstated, ... Which one should I use?',
+    userName: 'Anthony',
+    created: new Date(),
+    answers: [],
+  },
+];
