9d0603a5e923dda8f6548bd2c3e266a9b9f94ca9
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-30 18:50:57 -0500

    Feat: Creating a function that will return all unanswered questions.

--- Ezenity_QandA/frontend/src/mockData/QuestionsData.ts
+++ Ezenity_QandA/frontend/src/mockData/QuestionsData.ts
@@ -48,3 +48,8 @@ const questions: QuestionData[] = [
     answers: [],
   },
 ];
+
+// Function that returns unanswered questions
+export const getUnansweredQuestions = (): QuestionData[] => {
+  return questions.filter(q => q.answers.length === 0);
+};
