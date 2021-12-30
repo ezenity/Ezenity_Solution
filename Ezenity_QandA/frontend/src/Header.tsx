a9877fcc0eb95dbcbbb9b52a13c318c83bd8fe6f
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-30 17:07:09 -0500

    Doc: info regarding keywords

--- Ezenity_QandA/frontend/src/Header.tsx
+++ Ezenity_QandA/frontend/src/Header.tsx
@@ -1,3 +1,5 @@
 ﻿import React from 'react';
 
+// const: Allows you to declare and initialize a variable where its reference won't change later in the program
+// let: allows you to delcare a variable where its reference can change later in the program
 export const Header = () => <div>header</div>;
