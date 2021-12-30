dfb43d84d978b77fecb4a1314c967e36ce1be7b8
DwDMadMac <anthonymmacallister@gmail.com>
2021-12-30 18:18:36 -0500

    Feat: Customize header component

--- Ezenity_QandA/frontend/src/Header.tsx
+++ Ezenity_QandA/frontend/src/Header.tsx
@@ -3,20 +3,20 @@ import logo from './logo.svg';
 
 // const: Allows you to declare and initialize a variable where its reference won't change later in the program
 // let: allows you to delcare a variable where its reference can change later in the program
+// ------------------------------------------------------------------------------------------------------------------
+// When an implicit return statement is on multiple lines it requires to have a parentheses, otherwise if it is on
+// a single line then you can use the following statement below.
 // export const Header = () => <div>Header<div/>
+
 export const Header = () => (
   <header className="App-header">
     <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload. Here is a edit.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
+    <div>
+      <a href="./">Q & A</a>
+      <input type="text" placeholder="Search.." />
+      <a href="./signin">
+        <span>Sign In</span>
+      </a>
+    </div>
   </header>
 );
