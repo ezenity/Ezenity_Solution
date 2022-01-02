b08a235d66a88107afcd83ee01d4f8a8f7bf726b
DwDMadMac <anthonymmacallister@gmail.com>
2022-01-02 18:27:44 -0500

    Feat: making use of Children Props

---
+++ Ezenity_QandA/frontend/src/Page.tsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+import { PageTitle } from './PageTitle';
+
+// This component is taking an optional title prop and
+// renders this inside the PageTitle component. The
+// component also takes in a children prop. 
+interface Props {
+  title?: string;
+  children: React.ReactNode;
+}
+
+// In the consuming component, the content nested within the page component
+// will be rendered where we have just placed the children prop.
+export const Page = ({ title, children }: Props) => (
+  <div>
+    {title && <PageTitle>{title}</PageTitle>}
+    {children}
+  </div>
+);
---
+++ Ezenity_QandA/frontend/src/PageTitle.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+
+// Define the children prop with a type annotation of ReactNode
+interface Props {
+  children: React.ReactNode;
+}
+
+
+// Here we are referencing the children prop inside the h2 element. 
+// This means that the child elements that consume components
+// specify will be placed inside the h2 element.
+export const PageTitle = ({ children, }: Props) => <h2>{children}</h2>;
