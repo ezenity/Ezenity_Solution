de18063c79a21792d4edcb9027964f65e337bd69
DwDMadMac <anthonymmacallister@gmail.com>
2022-01-03 18:17:18 -0500

    Feat: Define Routes
    - Define all of the routes to the pages I just created

--- Ezenity_QandA/frontend/src/App.tsx
+++ Ezenity_QandA/frontend/src/App.tsx
@@ -3,22 +3,33 @@ import { css } from '@emotion/react';
 import { fontFamily, fontSize, gray2 } from './assets/css/Styles';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 
 function App() {
   //const App: React.FC = () => {
   //const unused = 'something';
   //debugger;
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="ask" element={<AskPage />} />
+          <Route path="signing" element={<SignInPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
   //};
 }
