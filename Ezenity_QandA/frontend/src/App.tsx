b84cc1d311081bf97a0ee9e85e964faa053cefca
DwDMadMac <anthonymmacallister@gmail.com>
2022-01-04 16:31:43 -0500

    Feat: Added another route for the SignInPage

--- Ezenity_QandA/frontend/src/App.tsx
+++ Ezenity_QandA/frontend/src/App.tsx
@@ -49,6 +49,7 @@ function App() {
             }
           />
           <Route path="signin" element={<SignInPage />} />
+          <Route path="login" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
