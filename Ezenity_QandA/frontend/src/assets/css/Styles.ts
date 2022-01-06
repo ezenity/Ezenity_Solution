c7f8a7ad0f1b929af018f49a7e32c38813c21355
DwDMadMac <anthonymmacallister@gmail.com>
2022-01-06 17:07:24 -0500

    Feat: Created styled components to be used with forms

--- Ezenity_QandA/frontend/src/assets/css/Styles.ts
+++ Ezenity_QandA/frontend/src/assets/css/Styles.ts
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const gray1 = '#383737';
@@ -69,4 +70,98 @@ export const PageTitleStyle = styled.h2`
   margin: 10px 0px 5px;
   text-align: center;
   text-transform: uppercase;
+`;
+
+/**
+ * Styled component that us used for a feildset inside our forms
+ */
+export const Fieldset = styled.fieldset`
+  margin: 10px auto 0 auto;
+  padding: 30px;
+  width: 350px;
+  background-color: ${gray6};
+  border-radius: 4px;
+  border: 1px solid ${gray5};
+  box-shadow: 0 3px 5px 0 rgba (0, 0, 0, 0.16);
+`;
+
+/**
+ * Styl4ed component for the field container
+ */
+export const FieldContainer = styled.div`
+  margin-bottom: 10px;
+`;
+
+/**
+ * Styled component for the label element
+ */
+export const FieldLabel = styled.label`
+  font-weight: bold;
+`;
+
+/**
+ * Common CSS properties that the field editor element will have
+ */
+const baseFieldCSS = css`
+  box-sizing: border-box;
+  font-family: ${fontFamily};
+  font-size: ${fontSize};
+  margin-bottom: 5px;
+  padding: 8px 10px;
+  border: 1px solid ${gray5};
+  border-radius: 3px;
+  color: ${gray2};
+  background-color: white;
+  width: 100px;
+  :focus {
+    outline-color: ${gray5};
+  }
+  :disabled {
+    background-color: ${gray6};
+  }
+`;
+
+/**
+ * Styled component for an input element with the Common CSS variable
+ */
+export const fieldInput = styled.input`
+  ${baseFieldCSS}
+  height: 100px;
+`;
+
+/**
+ * Styled component for the textarea element with the Common CSS variable
+ */
+export const fieldTextArea = styled.textarea`
+  ${baseFieldCSS}
+  height: 100px;
+`;
+
+/**
+ * Styled component for the validation error message
+ */
+export const FieldError = styled.div`
+  font-size: 10px;
+  color: red;
+`;
+
+/**
+ * Styled compoment for a container for the form  submit button
+ */
+export const FormButtoncontainer = styled.div`
+  margin: 30px 0px 0px 0px;
+  padding: 20px 0px 0px 0px;
+  border-top: 1px solid ${gray5};
+`;
+
+/**
+ * Styled component for the submission messages
+ */
+export const SubmissionSuccess = styled.div`
+  margin-top: 10px;
+  color: green;
+`;
+export const SubmissionFailure = styled.div`
+  margin-top: 10px;
+  color: red;
 `;
