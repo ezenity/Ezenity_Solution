1eda4c9643488418179f609fc1a24340b4a1c5d3
DwDMadMac <anthonymmacallister@gmail.com>
2022-01-06 22:22:09 -0500

    Feat: Creating Ask Form
    - Made the component have an explicit return statement
    - Added destructure regsiter function
    - passed in the FormData Type, which uses the 'useForm' hook
    - Added a attribute which will read out the label using a screen reader when the input has focus

--- Ezenity_QandA/frontend/src/component/AskPage.tsx
+++ Ezenity_QandA/frontend/src/component/AskPage.tsx
@@ -1,5 +1,53 @@
 import React from 'react';
 import { Page } from './Page';
+import {
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldInput,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+} from '../assets/css/Styles';
+import { useForm } from 'react-hook-form';
 
-export const AskPage = () => <Page title="Ask a question">{null}</Page>;
+/**
+ * Represents the form data
+ */
+type FormData = {
+  title: string;
+  content: string;
+};
+
+export const AskPage = () => {
+  const { register } = useForm<FormData>();
+  return (
+    <Page title="Ask a question">
+      <form>
+        <Fieldset>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">Title</FieldLabel>
+            <FieldInput
+              id="title"
+              name="title"
+              type="text"
+              ref={register}
+            ></FieldInput>
+          </FieldContainer>
+          <FieldContainer>
+            <FieldLabel htmlFor="content">Content</FieldLabel>
+            <FieldTextArea
+              id="content"
+              name="content"
+              ref={register}
+            ></FieldTextArea>
+          </FieldContainer>
+          <FormButtonContainer>
+            <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
+          </FormButtonContainer>
+        </Fieldset>
+      </form>
+    </Page>
+  );
+};
 export default AskPage;
